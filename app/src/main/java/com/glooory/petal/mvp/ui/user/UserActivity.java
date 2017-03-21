a1a0d7115197d6a108f6c36f73a9e08125b2f9cf
Glooory <glooorypu@gmail.com>
2017-03-21 17:02:24 +0800

    Add UserActivity layout files

---
+++ app/src/main/java/com/glooory/petal/mvp/ui/user/UserActivity.java
@@ -0,0 +1,35 @@
+package com.glooory.petal.mvp.ui.user;
+
+import com.glooory.petal.R;
+
+import common.AppComponent;
+import common.PEActivity;
+
+/**
+ * Created by Glooory on 17/3/21.
+ */
+
+public class UserActivity extends PEActivity {
+
+
+
+    @Override
+    protected void setupActivityComponent(AppComponent appComponent) {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_user;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
